ca9cabdd0dbd757fca060f00bc10b9dabf1b014f
Yanely Garcia <garciayaneli045@gmail.com>
2021-02-25 12:15:25 -0600

    tarea Yanely 25 feb 2021

---
+++ yanelyGarcia/Par o impar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+int num;
+cout<<"Ingrese Numero :200 ";
+cin>>num;
+if(num%2==0)
+cout<<"El Numero es Par";
+else
+cout<<"El Numero es Impar";
+return 0;
+}
