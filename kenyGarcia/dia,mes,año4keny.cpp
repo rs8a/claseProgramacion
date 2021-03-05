ff3e809f914e52a7bf0e1733e717983493a4e8a5
Alexandra Garcia <kalexandrag504@gmail.com>
2021-03-04 22:41:36 -0600

    Tarea Keny Garcia 04 mar 2021

---
+++ kenyGarcia/dia,mes,año4keny.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 	int anio,mes,dia;
+ 	int fecha;//aaaammdd;
+ 	cout<<"ingrese, anio, mes, el dia"<<endl;
+ 	cin>>anio>>mes>>dia;
+	
+	cout<<dia<<"/"<<mes<<"/"<<anio<<endl;
+	cout<<fecha<<endl;
+
+ 	return 0;
+	
+}
